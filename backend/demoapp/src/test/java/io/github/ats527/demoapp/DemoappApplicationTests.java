2e688cc186cb6132b29b51bbe6fa6611ac5bf5bf
ats527 <ats527@github.com>
2026-05-01 16:13:02 +0530

    feat: Initialize demo application with Maven setup and basic structure

---
+++ backend/demoapp/src/test/java/io/github/ats527/demoapp/DemoappApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ats527.demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
